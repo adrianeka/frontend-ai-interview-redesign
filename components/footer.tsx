f6e898139a604d9e29bef4a3ff4bdb57debf7247
rihapfirdaus <muhammadrihap448@gmail.com>
2026-05-20 11:10:33 +0700

    style(layout): update minor style for footer and navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,12 @@
+import Image from "next/image";
+
 export function Footer() {
   return (
-    <footer className="border-t bg-background py-2 mt-5">
+    <footer className="border-t bg-[#FAFAFA] py-4 px-16">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between gap-4">
-        <div className="flex items-center gap-2">
-          <div className="flex h-[44px] w-[120px] items-center justify-center">
-            <img src='https://cdn.sejutacita.id/677f6599d39d490013975af8/JobPortalCompanyLogo/8e0deb93-22d0-4e14-ac8c-d3178eb4eada.png' />
-          </div>
-          <p className="text-xs text-muted-foreground font-medium">
+        <div className="flex items-center gap-2 w-full">
+          <Image src="/Logo.png" alt="Logo P79" width={120} height={44} className="w-[120px] h-[44px] object-cover" />
+          <p className="text-sm text-[#667085]">
             © 2026 AI Interview Platform • Built by Mini Padepokan 79 | All rights reserved.
           </p>
         </div>
